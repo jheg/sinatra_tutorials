7552c863445d432c5fb2790557b356b78f42a2c2
jheg <jason@jheg.co.uk>
2014-12-04 21:44:41 +0000

    adding myapp dir

--- myapp.rb
+++ myapp.rb
@@ -8,10 +8,31 @@ require 'sinatra'
 # end
 
 
-get '/foo.html' do
-  "Hello #{params[:name]}!!!!"
+# get '/foo.html' do
+#   "Hello #{params[:name]}!!!!"
+# end
+
+# get '/hello/:name' do
+#   "Hello #{params[:name]}!"
+# end
+
+# set(:probability) {|value| condition { rand <= value } }
+
+# get '/win_a_car', :probability => 0.1 do 
+#   "You won!"
+# end
+
+# get '/win_a_car' do
+#   "You did NOT win"
+# end
+
+class Stream
+  def each
+    10.times do |i| 
+      yield "#{i}\n hi!!!!!!" 
+    end
+  end
 end
 
-get '/hello/:name' do
-  "Hello #{params[:name]}!"
-end
+get('/') { Stream.new }
+
